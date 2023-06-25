8fda2eb7e64b4bde868beb72440e70aed21faadf
menmozhii <mozhieee@gmail.com>
2023-06-25 17:52:09 +0530

    first commit

---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+ const Card =()=>{
+    return(
+        <div className ="cardcontainer">
+            <h1>my first react site</h1>
+            <label htmlFor="sometext">sometext</label>
+            <input type ="text" id="sometext"></input>
+        </div>
+    );
+}
+export default Card;
